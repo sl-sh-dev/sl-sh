6f8a901836f6f0f86a7e9fcbabecb07687234352
Steven Stanfield <stanfield@scarecrowtech.com>
2019-09-02 22:39:18 -0400

    Hook up redimentry file completion.

--- src/completions.rs
+++ src/completions.rs
@@ -1,9 +1,19 @@
-use liner::Completer;
+use liner::{Completer, FilenameCompleter};
+use std::env;
 
 pub struct ShellCompleter;
 
 impl Completer for ShellCompleter {
-    fn completions(&mut self, _start: &str) -> Vec<String> {
-        Vec::new()
+    fn completions(&mut self, start: &str) -> Vec<String> {
+        match env::current_dir() {
+            Ok(p) => {
+                let mut fc = FilenameCompleter::new(Some(p));
+                fc.completions(start)
+            }
+            Err(err) => {
+                eprintln!("Error getting current working directory: {}", err);
+                Vec::new()
+            }
+        }
     }
 }
