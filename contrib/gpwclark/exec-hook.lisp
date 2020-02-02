499b4edbfcdff44c141a10c61cc2109b25555a17
gpwclark <gpwclark@gmail.com>
2020-02-02 04:46:10 +0000

    parse infix notation that occurs in subforms

--- contrib/gpwclark/exec-hook.lisp
+++ contrib/gpwclark/exec-hook.lisp
@@ -12,10 +12,9 @@
 ;; TODOS
 ;; 1. support things like $2la for second to last arg, $3la, etc..
 ;; 2. organize fcns
-;; 3.support nested infix?
-;; 4. how to handle mixing infix notation?
-;; cat file | grep "stuff" out> afile
-;; 5. use docstrings
+;; 3. how to handle mixing infix notation, e.g.
+;;		cat file | grep "stuff" out> afile
+;; 3. use docstrings
 
 
 	(defn change-dir-if-arg-is-dir (cmd)
@@ -24,13 +23,13 @@
 				(list root::cd cmd-str)
 				cmd-str)))
 
-    (defn apply-ast-order (ast order)
-        (if (< (length ast) 3)
-          (err "Expressions with infix symbols must have at least 3 forms")
-          (match order
-            (:as-is ast)
-            (:swap-last-for-first (append (list (first ast)) (last ast) (butlast (rest ast))))
-            (nil (err "Unable to apply ordering, unknown order symbol.")))))
+	(defn apply-ast-order (ast order)
+		(if (< (length ast) 3)
+			(err "Expressions with infix symbols must have at least 3 forms")
+			(match order
+				(:as-is ast)
+				(:swap-last-for-first (append (list (first ast)) (last ast) (butlast (rest ast))))
+				(nil (err "Unable to apply ordering, unknown order symbol.")))))
 
 	(defn prefixify-cmd (cmd-toks prefix-props)
 		(progn
@@ -50,29 +49,27 @@
 											cmd-ast
 											(vec (make-vec)))
 											cmd-ast)
-									(progn (append! (last cmd-ast) (vec next-tok)) cmd-ast))
+									(progn
+										(append!
+											(last cmd-ast)
+											(if (seq? next-tok) next-tok (vec next-tok)))
+										cmd-ast))
 								(rest raw-list)))))))
 			(defq prefixified-ast
 				(build-cmd (vec (hash-get prefix-props :prefix-symbol) (make-vec))
 				cmd-toks))
-            (setq prefixified-ast (apply-ast-order prefixified-ast (hash-get prefix-props :ast-order)))
-			;;(println "resultant-prefix-notation: " prefixified-ast)
+			(setq prefixified-ast (apply-ast-order prefixified-ast (hash-get prefix-props :ast-order)))
 			prefixified-ast))
 
 	;; return true if cmd satisfies preconditions for prefixification
 	(defn satisfies-prefixify-preconditions (cmd-ast infix-hash-set)
 		(let ((infix-symbol (hash-get infix-hash-set :infix-symbol)))
 			(not (or ;; reasons to skip pre-processing
-
 				;; if read returns nil
 				(not cmd-ast)
-
 				;; if cmd doesn't contain symbol
 				(not (in? cmd-ast infix-symbol))
-
 				;; if already in prefix notation
-				;;TODO edge case, there are more infix-symbols in ast? see note
-                ;; in larger TODO block at top.
 				(= infix-symbol (first cmd-ast))))))
 
 	(defn confirm-prefix-eligible (cmd-ast prefix-metadata)
@@ -134,19 +131,23 @@
 				cmd-ast
 				(prefixify-cmd cmd-ast prefix-eligible))))
 
-#|
-	(defn check-form-and-subforms-for-infix-notation (cmd-ast)
+	(defn recursively-check-for-infix-notation (new-ast orig-ast)
 		(progn
-            (defq fst (first cmd-ast))
-          (if (nil? fst)
-            cmd-ast
-            (if (list? cmd-ast)
-                  ))))
-|#
+			(defq fst (first orig-ast))
+			(defq rst (rest orig-ast))
+			(if (not fst)
+				(check-for-infix-notation new-ast)
+				(recur (append new-ast
+							(if (non-empty-seq? fst)
+								(progn
+									(defq prefixified-subform (check-for-infix-notation fst))
+									(vec (recursively-check-for-infix-notation (make-vec) prefixified-subform)))
+								(vec fst)))
+					rst))))
 
 	(defn __exec_hook (cmd-str)
 		(let ((cmd-ast (read :add-parens cmd-str)))
 				(match (length cmd-ast)
 					(1 (change-dir-if-arg-is-dir (first cmd-ast)))
-					(nil (check-for-infix-notation cmd-ast)))))
+					(nil (recursively-check-for-infix-notation (make-vec) cmd-ast)))))
 ;; }}}
