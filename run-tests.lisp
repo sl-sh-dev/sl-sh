929eda9af9e58a5d9a4cd4c532011464f27293f6
gpwclark <gpwclark@gmail.com>
2020-03-31 15:35:38 +0000

    use line-no

--- run-tests.lisp
+++ run-tests.lisp
@@ -12,21 +12,39 @@
 
 (defq test-list
 	(reduce
-		(fn (lst filename) (append! lst (fn () (load (str tests-dir "/" filename)))))
+		(fn (lst filename) (append! lst (progn
+										(defq name (str tests-dir "/" filename))
+										(defq load-fcn (fn () (load filepath)))
+										(defq test-hash (make-hash))
+										(hash-set! test-hash :name name)
+										(hash-set! test-hash :load-fcn load-fcn)
+										test-hash)))
 		'()
 		(filter
 		(fn (filename) (not (= filename "test.lisp")))
 			(str-split " " (all-items-by-whitespace (fn () (ls tests-dir)))))))
 
+(defn print-until (reader printer)
+	(progn
+	(defq line (read-line reader))
+	(when line
+		(progn
+			(printer line)
+			(recur reader printer)))))
+
+(defn print-each-line (file printer)
+	(progn
+	(defq reader (open file :read))
+	(print-until reader printer)))
+
 (defn print-test-output (outfile)
 	(progn
-		(defq output (str (cat outfile)))
-		(when (not (str-empty? output))
-			(println (str
-				shell::*fg-black* shell::*bg-white*
-				(str-trim output)
-				shell::*fg-default* shell::*bg-default*
-					 )))))
+	(defq printer (fn (output)
+		(println (str
+			shell::*fg-black* shell::*bg-white*
+			(str-trim output)
+			shell::*fg-default* shell::*bg-default*))))
+	(print-each-line outfile printer)))
 
 (defn report-pretty-printer (pass test-name)
 	(if pass
@@ -41,25 +59,26 @@
 			shell::*fg-default* shell::*bg-default*
 			" " test-name))))
 
-
 ;; use internal state of dyn, if it gets a non zero error code
 ;; we know the test failed.
-
 (defn report-test-results (tests)
 (progn
 	(defq exit-status #t)
-	(dyn 'exit (fn (x) (progn (println (str "mm: " (line-no))) (when (not (= x "0")) (setq exit-status nil)) x))
+	(dyn 'exit (fn (x) (progn
+				(echo -n (str ". line number: " (line-no) "."))
+				(when (not (= x "0")) (setq exit-status nil))
+				x))
 	(progn
-		(defq fst (first tests))
-		(when fst
+	(defq fst (first tests))
+	(when fst
+		(progn
 			(progn
-				(progn
-					;; TODO only make 1 outfile
-					(defq outfile (str (mktemp "/tmp/sl-sh.test-log.XXXXXXXXX")))
-					(out-err> outfile (fst))
-					(report-pretty-printer exit-status fst)
-					(print-test-output outfile)
-					(rm outfile)
-					(recur (rest tests)))))))))
+				;; TODO only make 1 outfile
+				(defq outfile (str (mktemp "/tmp/sl-sh.test-log.XXXXXXXXX")))
+				(out-err> outfile (:load-fcn fst))
+				(report-pretty-printer exit-status (:name fst))
+				(print-test-output outfile)
+				(rm outfile)
+				(recur (rest tests)))))))))
 
 (report-test-results test-list)
