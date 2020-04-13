5f27181be8d4bfc1d2c015d06d5750e961ef59a2
gpwclark <gpwclark@gmail.com>
2020-04-13 03:36:43 +0000

    running through some core functions

--- run-tests.lisp
+++ run-tests.lisp
@@ -10,41 +10,45 @@
 (defn all-items-by-whitespace (producer)
 	(str-trim (str (| (producer) (tr "\n" " ") (tr -s ":blank:")))))
 
-(defq test-list
+(defn make-test-list-from-symbols (symbols-list)
+  (progn
+	(defq test-list '())
+	(for sym symbols-list (progn
+	(if (not (or (str-starts-with "ns-" (str sym))(= "*ns*" (str sym)))) (progn
+		;;(set 'total-syms (+ 1 total-syms))
+		(if (str-contains "Example:" (doc (to-symbol (str "root::" sym))))
+			(progn
+				;;(set 'total-tests (+ 1 total-tests))
+				;;(print sym " ")
+				(defq test-set-item (make-hash))
+				(hash-set! test-set-item :name (str sym))
+				(hash-set! test-set-item :load-fcn (fn () (test::run-example (to-symbol (str "root::" sym)))))
+				(append! test-list test-set-item))
+			(progn
+				(defq test-set-item (make-hash))
+				(hash-set! test-set-item :name (str sym))
+				(hash-set! test-set-item :load-fcn :no-test)))))))
+	test-list))
+
+(defq file-test-list
 	(reduce
 		(fn (lst filename) (append! lst (progn
-										(defq name (str tests-dir "/" filename))
-										(defq load-fcn (fn () (load name)))
-										(defq test-hash (make-hash))
-										(hash-set! test-hash :name name)
-										(hash-set! test-hash :load-fcn load-fcn)
-										test-hash)))
+			(defq name (str tests-dir "/" filename))
+			(defq load-fcn (fn () (load name)))
+			(defq test-set-item (make-hash))
+			(hash-set! test-set-item :name name)
+			(hash-set! test-set-item :load-fcn load-fcn)
+			test-set-item)))
 		'()
 		(filter
 		(fn (filename) (not (= filename "test.lisp")))
 			(str-split " " (all-items-by-whitespace (fn () (ls tests-dir)))))))
 
-(defn print-until (reader printer)
-	(progn
-	(defq line (read-line reader))
-	(when line
-		(progn
-			(printer line)
-			(recur reader printer)))))
-
-(defn print-each-line (file printer)
-	(progn
-	(defq reader (open file :read))
-	(print-until reader printer)))
-
-(defn print-test-output (outfile)
-	(progn
-	(defq printer (fn (output)
-		(println (str
-			shell::*fg-black* shell::*bg-white*
-			(str-trim output)
-			shell::*fg-default* shell::*bg-default*))))
-	(print-each-line outfile printer)))
+(defn printer (output)
+	(println (str
+		shell::*fg-black* shell::*bg-white*
+		(str-trim output)
+		shell::*fg-default* shell::*bg-default*)))
 
 (defn report-pretty-printer (pass test-name)
 	(if pass
@@ -59,26 +63,59 @@
 			shell::*fg-default* shell::*bg-default*
 			" " test-name))))
 
+(defmacro inc (item)
+	`(setq ,item (+ ,item 1)))
+
 ;; use internal state of dyn, if it gets a non zero error code
 ;; we know the test failed.
 (defn report-test-results (tests)
 (progn
 	(defq exit-status #t)
+	(defq total 0)
+	(defq passed 0)
+	(defq failed 0)
 	(dyn 'exit (fn (x) (progn
-				(echo -n (str ". line number: " (line-no) "."))
-				(when (not (= x "0")) (setq exit-status nil))
+				(setq total (+ 1 total))
+				(if (not (= x "0"))
+					(progn
+						(setq exit-status nil))
+						(inc passed)
+				(inc failed))
 				x))
 	(progn
 	(defq fst (first tests))
 	(when fst
 		(progn
 			(progn
-				;; TODO only make 1 outfile
-				(defq outfile (str (mktemp "/tmp/sl-sh.test-log.XXXXXXXXX")))
-				(out-err> outfile ((hash-get fst :load-fcn)))
+				((hash-get fst :load-fcn))
 				(report-pretty-printer exit-status (hash-get fst :name))
-				(print-test-output outfile)
-				(rm outfile)
 				(recur (rest tests)))))))))
 
-(report-test-results test-list)
+				;;(hash-set! test-report :total total)
+				;;(hash-set! test-report :passed passed)
+				;;(hash-set! test-report :failed failed)
+
+;;TODO handle :no-test form keyword.
+(defq final-test-report '())
+
+(printer "Tests from test directory")
+
+;;(defq file-test-report (make-hash))
+;;(hash-set! file-test-report :name "tests")
+;;(hash-set! file-test-report :report (make-hash))
+;;(report-test-results file-test-list (hash-get file-test-report :report))
+(report-test-results file-test-list)
+;;(append! final-test-report file-test-report)
+
+;;TODO de-dupe this pattern
+(printer "Tests from root namespace")
+(defq root-list (qsort (ns-symbols 'root)))
+(defq root-list (make-test-list-from-symbols root-list))
+;;(defq root-test-report (make-hash))
+;;(hash-set! root-test-report :name "root namespace tests")
+;;(hash-set! root-test-report :report (make-hash))
+;;(report-test-results root-list (hash-get root-test-report :report))
+(report-test-results root-list)
+;;(append! final-test-report file-test-report)
+
+;;(println final-test-report)
