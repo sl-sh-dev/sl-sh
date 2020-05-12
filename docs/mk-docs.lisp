bf7ff79322f4d33795c55aeb812fd3ca3b836d70
gpwclark <gpwclark@gmail.com>
2020-05-12 06:07:56 +0000

    reading md post and extracting appropriate metadata

--- docs/mk-docs.lisp
+++ docs/mk-docs.lisp
@@ -67,6 +67,81 @@
 		(map! (fn (x) (doc (to-symbol x)))
 			  (get-doc-list-for target-doc-form))))
 
-(ns-export '(make-md-file get-doc-list-for make-md-file-with-docstrings))
+(defn make-post
+	"execute in docs dir, create a new post. pass in title of post, the header,
+	for the post, and optionally, cateogires.
+	Section: scripting"
+	(title header &rest categories) (progn
+	(defq post-body (str "---
+layout: default
+title: " title "
+categories: [" (if (= 0 (length categories)) "general" (str-cat-list "," categories)) "]
+---
+# " header "
+<hr>
+[<-- back to the docs]( {{ site.url }} )
+"))
+	(defq date (str-trim (str (date +%Y-%m-%d))))
+	(defq post (open (str "_posts/" date "-" (str-replace title " " "-") ".md") :create :append))
+	(write-line post post-body)
+	(close post)))
 
+(defq begin-comment "{% comment %}")
+(defq end-comment "{% endcomment %}")
+(defq code-block-delim "```")
+
+(defn -get-directive-hash (file) (block file-read
+	(defq line (read-line file))
+	(when (nil? line)
+		(return-from file-read nil))
+	(if (and
+			(str-contains begin-comment (line))
+			(str-contains end-comment (line)))
+		(progn
+			(defq directive-metadata (vec-nth 1 (str-split begin-comment (vec-nth 0 (str-split end-comment line)))))
+			(eval (read directive-metadata))
+			(hash-set! do :trigger-line line)
+			do)
+		(progn
+			(recur file)))
+))
+
+(defn -read-in-code-block (file directive-map code-snippets) (progn
+	(defq line (str-trim (read-line file)))
+	(println "line is::: " line)
+	(defq contents (list))
+	(when (not (= line code-block-delim))
+		(err "First line after :entrypoint or :lib directive must be ```"))
+	;; read code block into list of strings, store list in directive map as
+	;; contents and return.
+	(loop (file contents) (file contents) (progn
+		(setq line (str-trim (read-line file)))
+		(println "line: " line)
+		(if (= line code-block-delim)
+			contents
+			(recur file (append! contents line)))))
+	(hash-set! directive-map :contents contents)
+	(hash-set! code-snippets (hash-get directive-map :name) directive-map)
+	directive-map))
+
+(defn -eval-file (file-name trigger-line code-snippets)
+	nil)
+
+(defn eval-post
+	"Section: scripting"
+	(file-name) (progn
+		(defq code-snippets (make-hash))
+		(defq file (open file-name :read))
+		(loop (file) (file) (progn
+			(defq directive-map (-get-directive-hash file))
+			(when (not (nil? directive-map)) (progn
+				(match (hash-get directive-map :type)
+					(:entrypoint (-read-in-code-block file directive-map code-snippets))
+					(:lib (-read-in-code-block file directive-map code-snippets))
+					(:eval (-eval-file file-name (hash-get directive-map :trigger-line) code-snippets))
+					(nil (err "Unknown hash map found")))
+				(recur file)))))))
+
+
+(ns-auto-export 'mkdocs)
 (ns-pop)
