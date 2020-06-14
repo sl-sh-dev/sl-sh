091705470d160e810f4a090bf7127ee1a65dbf10
Steven Stanfield <stanfield@scarecrowtech.com>
2020-06-13 23:59:14 -0400

    build.rs can handle missing git and add compiler to version string.

--- build.rs
+++ build.rs
@@ -9,20 +9,42 @@ const BUILD_TYPE: &str = "debug";
 const BUILD_TYPE: &str = "release";
 
 fn main() {
-    let version_string = format!(
-        "{} {} ({}:{}{}, {} build, {} [{}], {} UTC)",
-        env!("CARGO_PKG_NAME"),
-        env!("CARGO_PKG_VERSION"),
-        get_branch_name(),
-        get_commit_hash(),
-        if is_working_tree_clean() { "" } else { "+" },
-        BUILD_TYPE,
-        OS,
-        ARCH,
-        Utc::now().format("%b %d %Y, %T")
-    );
+    let version_string = if have_git() {
+        format!(
+            "{} {} ({}:{}{}, {} build, {} [{}], {} UTC [{}])",
+            env!("CARGO_PKG_NAME"),
+            env!("CARGO_PKG_VERSION"),
+            get_branch_name(),
+            get_commit_hash(),
+            if is_working_tree_clean() { "" } else { "+" },
+            BUILD_TYPE,
+            OS,
+            ARCH,
+            Utc::now().format("%b %d %Y, %T"),
+            get_rustc_version(),
+        )
+    } else {
+        format!(
+            "{} {} ({} build, {} [{}], {} UTC [{}])",
+            env!("CARGO_PKG_NAME"),
+            env!("CARGO_PKG_VERSION"),
+            BUILD_TYPE,
+            OS,
+            ARCH,
+            Utc::now().format("%b %d %Y, %T"),
+            get_rustc_version(),
+        )
+    };
 
     println!("cargo:rustc-env=VERSION_STRING={}", version_string);
+}
+
+fn have_git() -> bool {
+    Command::new("git")
+        .arg("--version")
+        .current_dir(env!("CARGO_MANIFEST_DIR"))
+        .output()
+        .is_ok()
 }
 
 fn get_commit_hash() -> String {
@@ -66,4 +88,18 @@ fn is_working_tree_clean() -> bool {
         .unwrap();
 
     status.code().unwrap() == 0
+}
+
+fn get_rustc_version() -> String {
+    let output = Command::new("rustc")
+        .arg("--version")
+        .current_dir(env!("CARGO_MANIFEST_DIR"))
+        .output()
+        .unwrap();
+
+    assert!(output.status.success());
+
+    String::from_utf8_lossy(&output.stdout)
+        .trim_end()
+        .to_string()
 }
