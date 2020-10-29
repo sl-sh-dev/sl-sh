afd0e68fb26b3e458d931543c6cc756e497cf2ab
gpwclark <gpwclark@gmail.com>
2020-10-29 06:23:13 +0000

    preliminary type support, need to enumerate more test cases for type checking and better error messages when type checking fails

--- getopts.lisp
+++ getopts.lisp
@@ -6,9 +6,10 @@
 
 ;; TODO make debugln use env var?
 ;; TODO do keys have to start with :-?
+;; TODO need some FAILING test cases for TYPES
 ;; TODO TYPES
 (defmacro debugln (&rest args)
-    (if (nil? nil)
+    (if (nil? #t)
         `(println "=> " ,@args)))
 
 (def 'sample "-la -c -b")
@@ -16,6 +17,8 @@
 (def 'token-delim "-")
 
 (def 'no-args "Getopts requires arguments.")
+
+(def 'options-map-is-map "Getopts first argument, options-map, must pass test hash?.")
 
 (def 'bad-first-arg "First argument must be a flag.")
 
@@ -106,11 +109,12 @@ up until the next token delimeted option, -c.
                    (verify-all-options-valid sub-vec options-map bindings-map)
                    "a"))))))
 
-(defn build-getopts-param (arity default)
+(defn build-getopts-param (arity default type-fun)
     (make-hash
         (list
         (join :arity arity)
-        (join :default default))))
+        (join :default default)
+        (join :type type-fun))))
 
 (defn valid-first-arg? (args)
     (when (not (is-getopts-option-string (first args)))
@@ -120,6 +124,21 @@ up until the next token delimeted option, -c.
 
 (defn make-hash-with-keys (hmap)
     (make-hash (collect (map (fn (x) (join (to-symbol x) nil)) (hash-keys hmap)))))
+
+(defn fix-one-arg-bindings
+"Ensure binding with arity of 1 is bound to actual value passed in, not
+a vector whose only element is the desired binding."
+      (options-map bindings-map)
+    (loop (keys bindings-map) ((hash-keys bindings-map) bindings-map)
+        (when (not (empty-seq? keys))
+            (progn
+             (var 'key (first keys))
+             (var 'opt-config (hash-get options-map key))
+             (var 'opt-arity (hash-get opt-config :arity))
+             (var 'binding (hash-get bindings-map key))
+             (when (and (not (nil? opt-arity)) (= 1 opt-arity) (seq? binding) (= 1 (length binding)))
+               (hash-set! bindings-map key (first binding)))
+             (recur (rest keys) bindings-map)))))
 
 (defn apply-defaults (options-map bindings-map)
     (loop (keys bindings-map) ((hash-keys options-map) bindings-map)
@@ -131,23 +150,89 @@ up until the next token delimeted option, -c.
                (hash-set! bindings-map key (hash-get opt-config :default)))
              (recur (rest keys) bindings-map)))))
 
-(defn fix-one-arg-bindings
-"Ensure binding with arity of 1 is bound to actual value passed in, not
-a vector whose only element is the desired binding."
-      (options-map bindings-map)
-    (loop (keys bindings-map) ((hash-keys bindings-map) bindings-map)
+(defn read-then-check-self (func)
+  (fn (x)
+    (var 'read-x (read x))
+    (var 'func-ret (func read-x))
+    ;; TOOD if we pass the test, then return the value? otherwise, throw a
+    ;; crappy error? we can throw a better error?
+    (if func-ret
+      read-x
+      (err "Bad types"))))
+
+(def 'supported-types-map
+    (make-hash
+      (list
+        (join 'int? (fn (x) (str->int x)))
+        (join 'float? (fn (x) (str->float x)))
+        (join 'file? (fn (x) (file? x)))
+        (join 'symbol? (fn (x) (to-symbol x)))
+        (join 'string? (fn (x) x))
+        (join 'char? (read-then-check-self char?))
+        (join 'func? (read-then-check-self func?))
+        (join 'builtin? (read-then-check-self builtin?))
+        (join 'macro? (read-then-check-self macro?))
+        (join 'hash? (read-then-check-self hash?))
+        (join 'nil? (read-then-check-self nil?))
+        (join 'pair? (read-then-check-self pair?))
+        (join 'lambda? (read-then-check-self lambda?))
+        (join 'list? (read-then-check-self list?))
+        (join 'vec? (read-then-check-self vec?))
+        (join 'non-empty-seq? (read-then-check-self non-empty-seq?))
+        (join 'empty-seq? (read-then-check-self empty-seq?)))))
+
+(def 'invalid-type-function (str "Type function must be one of " (hash-keys supported-types-map)))
+
+(defn enforce-types (options-map bindings-map)
+    (loop (keys bindings-map) ((hash-keys options-map) bindings-map)
         (when (not (empty-seq? keys))
             (progn
              (var 'key (first keys))
              (var 'opt-config (hash-get options-map key))
-             (debugln "optconfig: " opt-config ", type: " (type opt-config))
-             (var 'opt-arity (hash-get opt-config :arity))
+             (var 'opt-type-arity (hash-get opt-config :arity))
+             (var 'opt-type-fun (hash-get opt-config :type))
              (var 'binding (hash-get bindings-map key))
-             (when (and (= 1 opt-arity) (seq? binding) (= 1 (length binding)))
-               (hash-set! bindings-map key (first binding)))
+             (when (and (not (nil? opt-type-arity))
+                        (not (nil? opt-type-fun)))
+               (progn
+                 (if (not (in? (hash-keys supported-types-map) opt-type-fun))
+                   (err invalid-type-function)
+                   (progn
+                     (when (not (= 0 opt-type-arity))
+                       (progn
+                         (debugln "look our binding is: " binding ", of type: " (type binding))
+                         (if (= 1 opt-type-arity)
+                             (hash-set! bindings-map key ((hash-get supported-types-map opt-type-fun) binding))
+                             (hash-set! bindings-map key (collect-vec (map (hash-get supported-types-map opt-type-fun) binding))))
+                         (debugln "look our binding is now: " (hash-get bindings-map key) ", of type: " (type (hash-get bindings-map key)))
+                         ))
+                     ))))
              (recur (rest keys) bindings-map)))))
 
-(defn getopts (options-map &rest args)
+(def 'test-options-map
+    (make-hash
+      (list
+        (join :-l (build-getopts-param 0 #t nil))
+        (join :-m (build-getopts-param 0 nil nil))
+        (join :-a (build-getopts-param 1 "foo" nil))
+        (join :--c-arg (build-getopts-param 1 '#("bar") nil))
+        (join :--d-arg (build-getopts-param 2 nil nil))
+        (join :-b (build-getopts-param 3 nil nil)))))
+
+(defn getopts
+"Pass in option-map of the form:
+
+$(str test-options-map)
+
+followed by an optstring client passed in from the command line.
+
+Supported type arguments: DO THE RELATIVE LINK THING
+
+$(str (hash-keys supported-types-map))
+"
+(options-map &rest args)
+    (when (not (hash? options-map))
+        (err options-map-is-map))
     (when (not (> (length args) 0))
         (err no-args))
     (valid-first-arg? args)
@@ -157,19 +242,12 @@ a vector whose only element is the desired binding."
     ;; (= arity 1) to be a sequence.
     (fix-one-arg-bindings options-map bindings-map)
     (apply-defaults options-map bindings-map)
+    (debugln "options " options-map)
+    (enforce-types options-map bindings-map)
     (debugln "bindings-map: " bindings-map)
     bindings-map)
 
-(def 'test-options-map
-    (make-hash
-      (list
-        (join :-l (build-getopts-param 0 #t))
-        (join :-m (build-getopts-param 0 nil))
-        (join :-a (build-getopts-param 1 "foo"))
-        (join :--c-arg (build-getopts-param 1 '#("bar")))
-        (join :--d-arg (build-getopts-param 2 nil))
-        (join :-b (build-getopts-param 3 nil)))))
-
+(assert-error-msg (getopts "a") options-map-is-map)
 (assert-error-msg (getopts test-options-map) no-args)
 (assert-error-msg (getopts test-options-map "a") bad-first-arg)
 (assert-error-msg (getopts test-options-map "abc") bad-first-arg)
@@ -215,6 +293,30 @@ a vector whose only element is the desired binding."
             (join :-m #t)
             (join :-a "foo")
             (join :--c-arg '#("bar"))
+            (join :--d-arg nil)
+            (join :-b '#("1" "2" "3"))))))
+
+(assert-true
+    (map=
+        (getopts test-options-map "-b" "1" "2" "3" "-lm" "--c-arg" "bar")
+        (make-hash
+          (list
+            (join :-l #t)
+            (join :-m #t)
+            (join :-a "foo")
+            (join :--c-arg "bar")
+            (join :--d-arg nil)
+            (join :-b '#("1" "2" "3"))))))
+
+(assert-false
+    (map=
+        (getopts test-options-map "-b" "1" "2" "3" "-lm")
+        (make-hash
+          (list
+            (join :-l #t)
+            (join :-m #t)
+            (join :-a "foo")
+            (join :--c-arg "bar")
             (join :--d-arg nil)
             (join :-b '#("1" "2" "3"))))))
 
@@ -290,6 +392,17 @@ a vector whose only element is the desired binding."
             (join :--d-arg '#("1" "2"))
             (join :-b '#("1" "2" "3"))))))
 
+(assert-true
+    (map=
+    (getopts test-options-map "-l")
+    (make-hash
+      (list
+        (join :-l #t)
+        (join :-m nil)
+        (join :-a "foo")
+        (join :--c-arg '#("bar"))
+        (join :--d-arg nil)
+        (join :-b nil)))))
 
 (assert-true
     (map=
@@ -302,5 +415,46 @@ a vector whose only element is the desired binding."
         (join :--c-arg '#("bar"))
         (join :--d-arg nil)
         (join :-b '#("1" "2" "3"))))))
+
+(def 'test-options-map-types
+    (make-hash
+      (list
+        (join :-l (build-getopts-param 0 #t nil))
+        (join :-m (build-getopts-param 0 nil nil))
+        (join :-a (build-getopts-param 1 "foo" nil))
+        (join :--c-arg (build-getopts-param 1 '#("bar") nil))
+        (join :--d-arg (build-getopts-param 2 nil nil))
+        (join :-b (build-getopts-param 3 nil nil)))))
+
+(assert-error-msg
+  (getopts
+    (make-hash
+      (list
+        (join :-l (build-getopts-param 0 #t 'true?))))
+    "-l") 
+  invalid-type-function)
+
+(assert-error-msg
+  (getopts
+    (make-hash
+      (list
+        (join :-l (build-getopts-param 0 #t 'true?))))
+    "-l")
+  invalid-type-function)
+
+(assert-error-msg (getopts (make-hash (list (join :-c (build-getopts-param 1 nil 'float?)))) "-c" "#\a") "str->float: string is not a valid float")
+
+(lex
+    (var 'f-float-bindings (hash-get (getopts (make-hash (list (join :-f (build-getopts-param 1 nil 'float?)))) "-f" "1.3") :-f))
+    (assert-true (float? f-float-bindings) ". Return value should be of type float.")
+
+    (var 'f-float-vec-bindings (hash-get (getopts (make-hash (list (join :-f (build-getopts-param 2 nil 'float?)))) "-f" "1.3" "0.12") :-f))
+    (for f in f-float-vec-bindings (assert-true (float? f) ". Return value should be of type float."))
+
+    (var 'c-char-bindings (hash-get (getopts (make-hash (list (join :-c (build-getopts-param 1 nil 'char?)))) "-c" "#\a") :-c))
+    (assert-true (char? c-char-bindings) ". Return value should be of type char.")
+
+    (assert-error-msg (getopts (make-hash (list (join :-c (build-getopts-param 1 nil 'char?)))) "-c" "'#(\"a\")") "Bad types")
+)
 
 (ns-pop) ;; must be last line
