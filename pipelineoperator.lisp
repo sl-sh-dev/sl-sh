aadf9e73f30d941b59050e07e90823e064be7f6c
gpwclark <gpwclark@gmail.com>
2021-02-27 20:27:22 +0000

    put chain-and in

--- pipelineoperator.lisp
+++ pipelineoperator.lisp
@@ -3,123 +3,66 @@
 (ns-import 'iterator)
 (ns-import 'test)
 
-;; TODO need tests and a docstring
+(defmacro chain-and
+"Evaluates each sexp, if true, inserts result of previous expression into place
+inidicated by the _ symbol. This macro is useful when nested actions
+need to take place but the operations should stop and return nil if any step
+evaluates to false.
 
-(defn verify-chain-when-args (arg0 args)
-      (vec-insert! args 0 arg0)
-      (for elem in args
-           (do
-             (if (empty-seq? elem)
-                 (err "All args must be non-empty sequences.")
-                 (when (not (= 2 (length elem)))
-                   (err "Each clause in chain-when args must be of length 2.")))))
-      #t)
+Section: Threading-macros
 
-(defn verify-chain-args (arg0 args)
-      (vec-insert! args 0 arg0)
+Example:
+(test::assert-false (chain-and \"howdy\" (string? _) (= _ \"howdy\")))
+(test::assert-true  (chain-and \"howdy\" (str _ \" partner\") (= _ \"howdy partner\")))
+
+(defn formatted? (alleged-time-str)
+    (let* ((do-fst (fn (tst start end)
+                     (fn (orig-string)
+                     (let ((result (get-error
+                         (chain orig-string
+                            (str-slice _ start end)
+                            (str->int _)
+                            (tst _)))))
+                     (if (= :ok (car result))
+                       (if (cdr result) orig-string nil)
+                       nil)))))
+           (valid-year? (do-fst (fn (x) (> x -1)) 0 4))
+           (valid-month? (do-fst (fn (x) (and (> x 0) (< x 13))) 4 6))
+           (valid-day? (do-fst (fn (x) (and (> x 0) (< x 32))) 6 8))
+           (valid-hour? (do-fst (fn (x) (and (> x 0) (< x 24))) 9 11))
+           (valid-minute? (do-fst (fn (x) (and (> x -1) (< x 60))) 11 13))
+           (valid-second? (do-fst (fn (x) (and (> x -1) (< x 60))) 13 15))
+           (is-zulu? (fn (orig-string) (= \"Z\" (str-slice orig-string 15 16)))))
+      (chain-and alleged-time-str
+            (valid-year? _)
+            (valid-month? _)
+            (valid-day? _)
+            (valid-hour? _)
+            (valid-minute? _)
+            (valid-second? _)
+            (is-zulu? _)
+            #t)))
+
+(test::assert-true (formatted? \"20210227T154705Z\"))
+(test::assert-false (formatted? \"20210227T154705P\"))
+(test::assert-false (formatted? \"2021022TT154705Z\"))
+(test::assert-false (formatted? \"20210237T154705Z\"))
+(test::assert-false (formatted? \"2021222TT154705Z\"))
+(test::assert-false (formatted? \"20210227T254705Z\"))
+(test::assert-false (formatted? \"20210227T158705Z\"))
+(test::assert-false (formatted? \"20210227T154795Z\"))"
+  (init arg0 &rest args)
+  (do
+      (if (not (nil? args))
+        (vec-insert! args 0 arg0)
+        (do
+          (set! args (vec arg0))))
       (for elem in args (when (empty-seq? elem)
          (err "All args must be non-empty sequences.")))
       #t)
-
-Section: core
-
-Example:
-(test::assert-equal 3 (count (list 1 3 5 2 4 10 2 4 88 2 1) 2))
-(test::assert-equal 0 (count (list 1 3 5 2 4 10 2 4 88 2 1) 42))"
-      (sequence item)
-    (let ((add-if-equals-item (fn (fst nxt) (if (= nxt item) (+ 1 fst) fst))))
-      (reduce add-if-equals-item 0 sequence)))
-
-
-(defmacro chain (init arg0 &rest args)
-  (verify-chain-args arg0 args)
-  (let* ((reducer (fn (fst nxt)
-          (substitute fst '_ nxt))))
-    (reduce reducer init args)))
-
-(test::assert-equal "Ordered sentence: I will become a properly worded statement."
-    (chain "a properly worded "
-           (str "will " "become " _)
-           (str "I " _ "statement.")
-           (str "Ordered sentence: " _)))
-
-;;(test::assert-equal 42
-;;   (chain 7 (% _ 4)))
-
-(defmacro and-let* (vals &rest let-body)
-    (let* ((rev (iterator::reverse vals))
-           ;; if there is no let-body the last binding is returned
-           (init
-             (if (empty-seq? let-body)
-               (let ((last-binding (next! rev)))
-                 (if (= 1 (length last-binding))
-                   (car last-binding)
-                   (if (= 2 (length last-binding))
-                     (cdr last-binding)
-                     (err "ERROR: invalid bindings on and-let*"))))
-               let-body)))
-        (car (iterator::reduce
-             (fn (fst nxt)
-                (if (= 1 (length nxt))
-                    `(if ,nxt
-                         ((fn () ,@fst))
-                         nil)
-                    (if (= 2 (length nxt))
-                        (let ((val (car nxt))
-                              (bind (cdr nxt)))
-                            (let ((binding (gensym)))
-                              `((let ((,binding ,@bind))
-                                (if ,binding
-                                  ((fn (,val) ,@fst) ,binding)
-                                   nil)))))
-                          (err "ERROR: invalid bindings on and-let*"))))
-             init
-             rev))))
-
-(test::assert-equal "charlie bravo alpha."
-                    (and-let* ((val (do (str "alpha.")))
-                               (other-val (do (str "bravo " val)))) (str "charlie " other-val)) "]")
-
-(test::assert-equal "alpha, bravo." (and-let* ((val (do
-                                         (str "bravo.")))
-                                  (other-val (do
-                                                 (str "alpha, " val))))))
-
-(test::assert-equal nil (and-let* ((val (do (str "alpha, ") nil))
-                                   (other-val (do (str "bravo " val))))))
-
-(defmacro chain-and (init arg0 &rest args)
-  (verify-chain-args arg0 args)
-  (let* ((rev-args (collect (reverse args)))
+  (let* ((rev-args (collect (iterator::reverse args)))
          (first-rev (first rev-args))
          (rest-rev (collect (append-to! (rest rev-args) (list init))))
          (reducer (fn (fst nxt)
                      `((fn (_) (when _ ,fst)) ,nxt))))
-        (reduce reducer first-rev rest-rev)))
-
-(test::assert-false (chain-and "howdy" (string? _) (= _ "howdy")))
-(test::assert-true  (chain-and "howdy" (str _ " partner") (= _ "howdy partner")))
-
-(defmacro chain-when (init arg0 &rest args)
-  (verify-chain-when-args arg0 args)
-  (let* ((reducer
-           (fn (fst nxt)
-             (let* ((if-true (car nxt))
-                    (test (car (cdr nxt))))
-               `((fn (_) (if ,if-true ,test _)) ,fst)))))
-        (reduce reducer init args)))
-
-(defn add-number-attributes (n)
-    (chain-when (make-hash)
-        ((not (= (% n 2) 0)) (hash-set! _ :property "odd"))
-        ((= (% n 2) 0) (hash-set! _ :property "even"))
-        ((= 0 n) (hash-set! _ :zero "zero"))
-        ((> n 0) (hash-set! _ :symmetry "positive"))
-        ((< n 0) (hash-set! _ :symmetry "negative"))))
-
-(test::assert-equal "odd" (hash-get (add-number-attributes 3) :property))
-(test::assert-equal "even" (hash-get (add-number-attributes 4) :property))
-(test::assert-false (hash-get (add-number-attributes 4) :zero))
-(test::assert-equal "positive" (hash-get (add-number-attributes 4) :symmetry))
-(test::assert-equal "negative" (hash-get (add-number-attributes -4) :symmetry))
-(test::assert-equal "zero" (hash-get (add-number-attributes 0) :zero))
+        (iterator::reduce reducer first-rev rest-rev)))
