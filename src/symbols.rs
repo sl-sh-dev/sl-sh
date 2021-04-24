76270c836881b84791836f26fadfecf583af5a8c
Steven Stanfield <stanfield@scarecrowtech.com>
2021-04-24 21:18:42 +0000

    Some tweaks to the core (+, -, /, *, %) functions.
    These are only in the root namespace now (not both math and root).
    They avoid some unneeded allocations for better performance as well as
    better int/float handling.

--- src/symbols.rs
+++ src/symbols.rs
@@ -8,7 +8,7 @@ use crate::builtins_edit::add_edit_builtins;
 use crate::builtins_file::add_file_builtins;
 use crate::builtins_hashmap::add_hash_builtins;
 use crate::builtins_io::add_io_builtins;
-use crate::builtins_math::add_math_builtins;
+use crate::builtins_math::*;
 use crate::builtins_namespace::add_namespace_builtins;
 use crate::builtins_pair::add_pair_builtins;
 use crate::builtins_rand::add_rand_builtins;
@@ -262,6 +262,7 @@ impl Namespace {
         let mut stats_data: HashMap<&'static str, (Expression, String)> = HashMap::new();
         add_builtins(interner, &mut data);
         add_system_builtins(interner, &mut data);
+        add_root_math_builtins(interner, &mut data);
         add_math_builtins(interner, &mut math_data);
         add_stats_builtins(interner, &mut stats_data);
         add_rand_builtins(interner, &mut data);
