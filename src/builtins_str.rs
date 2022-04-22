1f2af960a22c8703ad68bea78f3f7201a178ca21
gpwclark <gpwclark@gmail.com>
2022-04-22 06:29:25 +0000

    unique colors for identical values in different capture groups

--- src/builtins_str.rs
+++ src/builtins_str.rs
@@ -412,22 +412,27 @@ fn rgb(r: u8, g: u8, b: u8) -> String {
     format!("\x1b[38;2;{};{};{}m", r, g, b)
 }
 
-fn color(str: &str) -> String {
+fn color(str: &str, capture_group: usize) -> String {
+    // can create unique colors for up to 32 capture groups
+    // given the bits from the str hash. as implemented means
+    // that identical values for the 1st and 33rd capture groups
+    // will have the same color.
+    let shift = capture_group % 32;
     let mut s = DefaultHasher::new();
     str.hash(&mut s);
     let hash = s.finish();
-    let r = (hash & 0xFF) as u8;
-    let g = (hash >> 8 & 0xFF) as u8;
-    let b = (hash >> 16 & 0xFF) as u8;
+    let r = (hash >> shift & 0xFF) as u8;
+    let g = (hash >> (shift + 8) & 0xFF) as u8;
+    let b = (hash >> (shift + 16) & 0xFF) as u8;
     rgb(r, g, b)
 }
 
-//TODO per capture group s.t. identical str's in different
-// capture groups can optionally be different colors,
-// just use hash and sample different bytes offset by capture
-// group...
-fn colorize_capture(str: &str) -> String {
-    format!("{}{}{}", color(str), str, FOREGROUND_DEFUALT)
+fn colorize_capture(str: &str, capture_group: usize, unique_colors: bool) -> String {
+    let mut capture_group = capture_group;
+    if !unique_colors {
+        capture_group = 0;
+    }
+    format!("{}{}{}", color(str, capture_group), str, FOREGROUND_DEFUALT)
 }
 
 fn regex_replace(sample: &str, regex: &Regex, replacement: &str) -> String {
@@ -438,18 +443,9 @@ fn get_regex_captures(sample: &str, regex: &Regex) -> Vec<Expression> {
     let mut capture_groups: Vec<Expression> = Vec::new();
     for caps in regex.captures_iter(sample) {
         let mut captures: Vec<Expression> = Vec::new();
-        if caps.len() > 1 {
-            for c in caps.iter().skip(1) {
-                if let Some(c) = c {
-                    captures.push(Expression::alloc_data(ExpEnum::String(
-                        c.as_str().to_string().into(),
-                        None,
-                    )));
-                }
-            }
-        } else {
+        for c in caps.iter().flatten() {
             captures.push(Expression::alloc_data(ExpEnum::String(
-                caps[0].to_string().into(),
+                c.as_str().to_string().into(),
                 None,
             )));
         }
@@ -462,11 +458,9 @@ fn regex_filter(sample: &str, regex: &Regex) -> String {
     let mut matches = String::from("");
     for caps in regex.captures_iter(sample) {
         if caps.len() > 1 {
-            for c in caps.iter().skip(1) {
-                if let Some(c) = c {
-                    matches = matches + c.as_str();
-                    matches = matches + " ";
-                }
+            for c in caps.iter().skip(1).flatten() {
+                matches += c.as_str();
+                matches += " ";
             }
         } else {
             matches = caps[0].to_string();
@@ -476,14 +470,12 @@ fn regex_filter(sample: &str, regex: &Regex) -> String {
     matches
 }
 
-
 //TODO
-// - colorize_string_with_regex_unique... makes same values that
-// occur in different capture groups different colors
+// - color palettes?
 // - colorize_string_from_group... colorize whole line based
 // on value from one capture group, possible do background instead
 // of foreground.
-fn colorize_string_with_regex(sample: &str, regex: &Regex) -> String {
+fn colorize_string_with_regex(sample: &str, regex: &Regex, unique_colors: bool) -> String {
     let mut offset = 0;
     regex
         .replace_all(sample, |caps: &Captures| {
@@ -506,21 +498,21 @@ fn colorize_string_with_regex(sample: &str, regex: &Regex) -> String {
                 for (idx, (start, end)) in offsets.iter().enumerate() {
                     let slice = &capture[*start..*end];
                     if idx == 0 && *start == 0 {
-                        strings.push(colorize_capture(slice));
+                        strings.push(colorize_capture(slice, idx, unique_colors));
                     } else if idx == 0 {
                         let begin = &capture[0..*start];
                         strings.push(begin.to_owned());
-                        strings.push(colorize_capture(&capture));
+                        strings.push(colorize_capture(&capture, idx, unique_colors));
                     } else if let Some(last_end) = last_end {
                         let begin = &capture[last_end..*start];
                         strings.push(begin.to_owned());
-                        strings.push(colorize_capture(slice));
+                        strings.push(colorize_capture(slice, idx, unique_colors));
                     }
                     last_end = Some(*end);
                 }
                 strings.join("")
             } else {
-                colorize_capture(&caps[0])
+                colorize_capture(&caps[0], 0, unique_colors)
             }
         })
         .into()
@@ -562,7 +554,11 @@ fn builtin_regex_replace(
     let replacement = &replacement.get().data;
     params_done(args, fn_name)?;
     match (regex, string, replacement) {
-        (ExpEnum::String(regex, _), ExpEnum::String(string, _), ExpEnum::String(replacement, _)) => {
+        (
+            ExpEnum::String(regex, _),
+            ExpEnum::String(string, _),
+            ExpEnum::String(replacement, _),
+        ) => {
             let regex = Regex::new(regex);
             match regex {
                 Ok(regex) => {
@@ -579,13 +575,15 @@ fn builtin_regex_replace(
             }
         }
         (ExpEnum::Regex(regex), ExpEnum::String(string, _), ExpEnum::String(replacement, _)) => {
-            let replaced = regex_replace(string, &regex, replacement);
+            let replaced = regex_replace(string, regex, replacement);
             Ok(Expression::alloc_data(ExpEnum::String(
                 replaced.into(),
                 None,
             )))
         }
-        (_, _, _) => Err(LispError::new("regex-replace takes a string, a regex, and a replacement string")),
+        (_, _, _) => Err(LispError::new(
+            "regex-replace takes a string, a regex, and a replacement string",
+        )),
     }
 }
 
@@ -603,18 +601,18 @@ fn builtin_regex_search(
         (ExpEnum::String(regex, _), ExpEnum::String(string, _)) => {
             let regex = Regex::new(regex);
             match regex {
-                Ok(regex) => {
-                    Ok(Expression::alloc_data(ExpEnum::Vector(get_regex_captures(string,&regex))))
-                }
+                Ok(regex) => Ok(Expression::alloc_data(ExpEnum::Vector(get_regex_captures(
+                    string, &regex,
+                )))),
                 Err(e) => Err(LispError::new(format!(
                     "regex-filter requires a valid regular expression.\n{}",
                     e
                 ))),
             }
         }
-        (ExpEnum::Regex(regex), ExpEnum::String(string, _)) => {
-            Ok(Expression::alloc_data(ExpEnum::Vector(get_regex_captures(string,&regex))))
-        }
+        (ExpEnum::Regex(regex), ExpEnum::String(string, _)) => Ok(Expression::alloc_data(
+            ExpEnum::Vector(get_regex_captures(string, regex)),
+        )),
         (_, _) => Err(LispError::new("regex-filter takes a string and a regex")),
     }
 }
@@ -666,13 +664,29 @@ fn builtin_regex_color(
     let regex = &regex.get().data;
     let string = param_eval(environment, args, fn_name)?;
     let string = &string.get().data;
+    let mut unique_colors = false;
+    if let Ok(unique) = param_eval(environment, args, fn_name) {
+        let unique = &unique.get().data;
+        if let ExpEnum::Symbol(str, _) = unique {
+            if *str == ":unique" {
+                unique_colors = true
+            } else if *str == ":default" {
+                unique_colors = false;
+            } else {
+                return Err(LispError::new(format!(
+                    "Optional third param to {} must be the symbol :unique or :default",
+                    fn_name
+                )));
+            }
+        }
+    }
     params_done(args, fn_name)?;
     match (regex, string) {
         (ExpEnum::String(regex, _), ExpEnum::String(string, _)) => {
             let regex = Regex::new(regex);
             match regex {
                 Ok(regex) => {
-                    let replaced = colorize_string_with_regex(string, &regex);
+                    let replaced = colorize_string_with_regex(string, &regex, unique_colors);
                     Ok(Expression::alloc_data(ExpEnum::String(
                         replaced.into(),
                         None,
@@ -685,7 +699,7 @@ fn builtin_regex_color(
             }
         }
         (ExpEnum::Regex(regex), ExpEnum::String(string, _)) => {
-            let replaced = colorize_string_with_regex(string, regex);
+            let replaced = colorize_string_with_regex(string, regex, unique_colors);
             Ok(Expression::alloc_data(ExpEnum::String(
                 replaced.into(),
                 None,
@@ -1968,7 +1982,8 @@ mod test {
         let sample = "2020-20-20 and then again on 2021-20-18 but not on 2020-18-20";
         let regex = "(\\d{4})-(\\d{2})-(\\d{2})";
         let regex = Regex::new(regex).unwrap();
-        let replaced = colorize_string_with_regex(sample, &regex);
+        let unique_colors = false;
+        let replaced = colorize_string_with_regex(sample, &regex, unique_colors);
         let expected = "\x1b[38;2;12;154;58m2020\x1b[39m-\x1b[38;2;103;93;109m20\x1b[39m-\x1b[38;2;103;93;109m20\x1b[39m and then again on \x1b[38;2;75;146;223m2021\x1b[39m-\x1b[38;2;103;93;109m20\x1b[39m-\x1b[38;2;217;27;83m18\x1b[39m but not on \x1b[38;2;12;154;58m2020\x1b[39m-\x1b[38;2;217;27;83m18\x1b[39m-\x1b[38;2;103;93;109m20\x1b[39m";
         assert_eq!(expected, replaced);
     }
