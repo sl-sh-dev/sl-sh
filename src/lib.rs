4c4bb5efac6e763c51aee4a99ba78a0b22a5f41f
gpwclark <gpwclark@gmail.com>
2022-07-09 21:27:28 +0000

    use static assertions to verify conversion traits are implemented

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,18 @@
 use proc_macro::TokenStream;
 use quote::quote;
 use quote::ToTokens;
-use std::any::Any;
 use std::error::Error;
 use std::fmt;
+use std::ops::Deref;
 use syn::__private::{Span, TokenStream2};
-use syn::{parse_macro_input, AttributeArgs, Field, GenericArgument, Ident, ItemFn, Lit, Meta, NestedMeta, PathArguments, ReturnType, Type, FnArg, TypePath, Path, PathSegment};
 use syn::punctuated::Punctuated;
-use syn::spanned::Spanned;
-use syn::token::{Colon2, Comma};
-
-fn get_inner_type<'a>(f: &'a Field, type_name: &str) -> Option<&'a GenericArgument> {
-    let ty = &f.ty;
-    match ty {
-        Type::Path(ref type_path) => {
-            if type_path.path.segments.len() == 1 {
-                //TODO fix calls to unwrap
-                let path_segment = &type_path.path.segments.first().unwrap();
-                let ident = &path_segment.ident;
-                if ident == type_name {
-                    match &path_segment.arguments {
-                        PathArguments::AngleBracketed(args) => {
-                            if args.args.len() == 1 {
-                                let ty = args.args.first().unwrap();
-                                Some(ty)
-                            } else {
-                                None
-                            }
-                        }
-                        _ => None,
-                    }
-                } else {
-                    None
-                }
-            } else {
-                None
-            }
-        }
-        _ => None,
-    }
-}
+use syn::token::Comma;
+use syn::{
+    parse_macro_input, AngleBracketedGenericArguments, AttributeArgs, FnArg, GenericArgument,
+    Ident, ItemFn, Lit, Meta, NestedMeta, Path, PathArguments, PathSegment, ReturnType, Type,
+    TypePath,
+};
+extern crate static_assertions;
 
 #[derive(Debug)]
 struct BuilderError {
@@ -67,66 +40,124 @@ impl Error for BuilderError {
     }
 }
 
-fn generate_builtin_arg_list(inputs: &Punctuated<FnArg, Comma>) -> (Vec<Ident>, Vec<Type>) {
-    let len = inputs.len();
+fn get_input_types(inputs: &Punctuated<FnArg, Comma>) -> Vec<Type> {
+    let mut types = vec![];
+    for input in inputs {
+        match input {
+            FnArg::Receiver(_) => {}
+            FnArg::Typed(ty) => {
+                types.push(ty.ty.deref().clone());
+            }
+        }
+    }
+    types
+}
+
+fn build_sl_sh_exp_enum_type() -> Type {
+    let sl_sh_path_segment = PathSegment {
+        ident: Ident::new("sl_sh", Span::call_site()),
+        arguments: PathArguments::None,
+    };
+    let exp_enum_path_segment = PathSegment {
+        ident: Ident::new("ExpEnum", Span::call_site()),
+        arguments: PathArguments::None,
+    };
+    let mut pun_seq = Punctuated::new();
+    pun_seq.push(sl_sh_path_segment);
+    pun_seq.push(exp_enum_path_segment);
+    Type::Path(TypePath {
+        qself: None,
+        path: Path {
+            leading_colon: None,
+            segments: pun_seq,
+        },
+    })
+}
+
+fn get_fn_names(fn_item: &ItemFn) -> (Ident, Ident) {
+    let sig_ident = &fn_item.sig.ident;
+    let name = sig_ident.to_string();
+    let builtin_name = "builtin_".to_string() + &name;
+    let builtin_name = Ident::new(&builtin_name, Span::call_site());
+    let original_fn_name = Ident::new(&name, Span::call_site());
+    (builtin_name, original_fn_name)
+}
+
+fn generate_builtin_arg_list(len: usize) -> (Vec<Ident>, Vec<Type>) {
     let mut fn_args = vec![];
     let mut fn_types = vec![];
     for i in 0..len {
-        let arg_name = "arg".to_string();
+        let arg_name = "arg_".to_string() + &i.to_string();
         let arg = Ident::new(&arg_name, Span::call_site());
         fn_args.push(arg);
-        let sl_sh_path_segment = PathSegment {
-            ident: Ident::new("sl_sh", Span::call_site()),
-            arguments: PathArguments::None,
-        };
-        let exp_enum_path_segment = PathSegment {
-            ident: Ident::new("ExpEnum", Span::call_site()),
-            arguments: PathArguments::None,
-        };
-        let mut pun_seq = Punctuated::new();
-        pun_seq.push(sl_sh_path_segment);
-        pun_seq.push(exp_enum_path_segment);
-        let ty: Type = Type::Path(TypePath {
-            qself: None,
-            path: Path {
-                leading_colon: None,
-                segments: pun_seq,
-            }
-        });
+        let ty = build_sl_sh_exp_enum_type();
         fn_types.push(ty);
     }
     (fn_args, fn_types)
 }
 
-fn verify_return_type(fn_item: &ItemFn) -> Type {
+fn wrap_with_std_convert(ty: Type, convert_trait: &str) -> Type {
+    let std_path_segment = PathSegment {
+        ident: Ident::new("std", Span::call_site()),
+        arguments: PathArguments::None,
+    };
+    let convert_path_segment = PathSegment {
+        ident: Ident::new("convert", Span::call_site()),
+        arguments: PathArguments::None,
+    };
+    let generic_argument = GenericArgument::Type(ty);
+    let mut generic_pun_seq = Punctuated::new();
+    generic_pun_seq.push(generic_argument);
+    let generic_argument = AngleBracketedGenericArguments {
+        colon2_token: None,
+        lt_token: Default::default(),
+        args: generic_pun_seq,
+        gt_token: Default::default(),
+    };
+    let trait_path_segment = PathSegment {
+        ident: Ident::new(convert_trait, Span::call_site()),
+        arguments: PathArguments::AngleBracketed(generic_argument),
+    };
+    let mut pun_seq = Punctuated::new();
+    pun_seq.push(std_path_segment);
+    pun_seq.push(convert_path_segment);
+    pun_seq.push(trait_path_segment);
+    Type::Path(TypePath {
+        qself: None,
+        path: Path {
+            leading_colon: None,
+            segments: pun_seq,
+        },
+    })
+}
+
+fn get_return_type(fn_item: &ItemFn) -> Type {
     let return_type = match &fn_item.sig.output {
         ReturnType::Default => {
             unimplemented!("Functions with attribute must return a value.");
         }
         ReturnType::Type(_ra_arrow, ty) => *ty.clone(),
     };
-    let supported_type = is_supported_sl_sh_type(&return_type);
-    if !supported_type.0{
-        unimplemented!("Unsupported return type {:?} for function", supported_type.1);
-    }
     return_type
 }
 
-fn is_supported_sl_sh_type(ty: &Type) -> (bool, String) {
-    match ty {
-        Type::Path(type_path) => {
-            let type_path = type_path.clone().into_token_stream().to_string();
-            return if type_path == "i64" || type_path == "f64" {
-                (true, type_path)
-            } else {
-                (false, type_path)
-            }
-        }
-        _ => {
-            let ts = ty.to_token_stream();
-            (false, ts.to_string())
-        }
+fn generate_assertions_code_for_type_conversions(fn_item: &ItemFn) -> Vec<TokenStream2> {
+    let inputs = &fn_item.sig.inputs;
+    let input_types = get_input_types(inputs);
+    let mut conversion_assertions_code = vec![];
+    for input_type in input_types {
+        let try_into = wrap_with_std_convert(input_type, "TryInto");
+        let exp_enum = build_sl_sh_exp_enum_type();
+        conversion_assertions_code.push(quote! {
+          static_assertions::assert_impl_all!(#exp_enum: #try_into);
+        });
     }
+    let return_type = get_return_type(&fn_item);
+    let to_return_type = wrap_with_std_convert(build_sl_sh_exp_enum_type(), "Into");
+    conversion_assertions_code.push(quote! {
+      static_assertions::assert_impl_all!(#return_type: #to_return_type);
+    });
+    conversion_assertions_code
 }
 
 fn get_attribute_value_with_key(key: &str, values: &[(String, String)]) -> Option<String> {
@@ -184,22 +215,20 @@ pub fn sl_sh_fn(attr: TokenStream, input: TokenStream) -> TokenStream {
         _ => unimplemented!("sl_sh_fn proc_macro_attribute only works on functions."),
     };
 
-    let return_type = verify_return_type(&fn_item);
-    let inputs = &fn_item.sig.inputs;
-    let (fn_args, fn_types) = generate_builtin_arg_list(inputs);
+    let conversions_assertions_code = generate_assertions_code_for_type_conversions(&fn_item);
 
-    let sig_ident = &fn_item.sig.ident;
-    let name = sig_ident.to_string();
-    let builtin_name = "builtin_".to_string() + &name;
-    let builtin_name = Ident::new(&builtin_name, Span::call_site());
-    let original_fn_name = Ident::new(&name, Span::call_site());
-    let original_fn = item.into_token_stream();
+    let (fn_args, fn_types) = generate_builtin_arg_list(fn_item.sig.inputs.len());
+    let (builtin_name, original_fn_name) = get_fn_names(&fn_item);
+    let original_fn_code = item.into_token_stream();
+
     let tokens = quote! {
-        #original_fn
+        #original_fn_code
+
         use std::convert::TryInto;
         use std::convert::TryFrom;
         fn #builtin_name(#(#fn_args: #fn_types),*) -> sl_sh::LispResult<sl_sh::types::Expression> {
-            let result: #return_type = #original_fn_name(#(#fn_args.try_into()?),*);
+            #(#conversions_assertions_code)*
+            let result = #original_fn_name(#(#fn_args.try_into()?),*);
             let result: ExpEnum = result.into();
             let #fn_name_attr = #fn_name;
             Ok(result.into())
