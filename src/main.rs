dae50327bd32d0216e0b8aa648ba94f9d532fabb
gpwclark <gpwclark@gmail.com>
2022-08-28 04:30:46 -0400

    optional arguments working

--- src/main.rs
+++ src/main.rs
@@ -18,11 +18,39 @@ extern crate static_assertions;
 fn main() -> Result<(), LispError> {
     let arg_0 = Expression::alloc_data(ExpEnum::Int(8));
     let arg_1 = Expression::alloc_data(ExpEnum::Int(8));
+    let exp = builtin_opt_int_to_float(sl_sh::ArgType::Exp(arg_0), sl_sh::ArgType::Opt(Some(arg_1))).unwrap();
+    let exp_d = exp.get();
+    match exp_d.data {
+        ExpEnum::Float(f) => {
+            assert_eq!(16.0, f);
+            println!("optional int to float working");
+        }
+        _ => {
+            panic!("Should befloat.");
+        }
+    }
+
+    let arg_0 = Expression::alloc_data(ExpEnum::Int(8));
+    let exp = builtin_opt_int_to_float(sl_sh::ArgType::Exp(arg_0), sl_sh::ArgType::Opt(None)).unwrap();
+    let exp_d = exp.get();
+    match exp_d.data {
+        ExpEnum::Float(f) => {
+            assert_eq!(8.0, f);
+            println!("tricky optional int to float working");
+        }
+        _ => {
+            panic!("Should befloat.");
+        }
+    }
+
+    let arg_0 = Expression::alloc_data(ExpEnum::Int(8));
+    let arg_1 = Expression::alloc_data(ExpEnum::Int(8));
     let exp = builtin_int_to_float(sl_sh::ArgType::Exp(arg_0), sl_sh::ArgType::Exp(arg_1)).unwrap();
     let exp_d = exp.get();
     match exp_d.data {
         ExpEnum::Float(f) => {
             assert_eq!(16.0, f);
+            println!("basic int to float working");
         }
         _ => {
             panic!("Should befloat.");
@@ -133,6 +161,14 @@ fn int_to_float(int: i64, ints: i64) -> sl_sh::LispResult<f64> {
     Ok((int + ints) as f64)
 }
 
+/// my docs
+#[sl_sh_proc_macros::sl_sh_fn2(fn_name = "optintofloat")]
+fn opt_int_to_float(int: i64, ints: Option<i64>) -> sl_sh::LispResult<f64> {
+    //Ok(ints.iter().fold(int as f64, |sum, next| sum + *next as f64))
+    Ok(ints.map_or(int as f64, |new| (int + new) as f64))
+}
+
+
 //fn builtin_int_to_float(
 //    exp_0: sl_sh::ArgType,
 //    exp_1: sl_sh::ArgType,
@@ -217,3 +253,89 @@ fn int_to_float(int: i64, ints: i64) -> sl_sh::LispResult<f64> {
 //        .fold(my_int as f64, |sum, val| sum + val as f64)
 //}
 //
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fn builtin_opt_int_to_float(
+        arg_0: sl_sh::ArgType,
+        arg_1: sl_sh::ArgType,
+    ) -> sl_sh::LispResult<Expression> {
+        match arg_1 {
+            sl_sh::ArgType::Opt(arg_1) => {
+                match arg_1 {
+                    None => {
+                        let arg_1 = None;
+                        match arg_0 {
+                            sl_sh::ArgType::Exp(arg_0) => {
+                                match arg_0.get().data {
+                                    sl_sh::ExpEnum::Int(arg_0) => {
+                                        opt_int_to_float(arg_0, arg_1).map(Into::into)
+                                    }
+                                    _ => {
+                                        return Err(
+                                            LispError::new(
+                                                "sl_sh_fn macro is broken, ArgType::Exp can't be parsed as ArgType::Exp",
+                                            ),
+                                        );
+                                    }
+                                }
+                            }
+                            _ => {
+                                return Err(
+                                    LispError::new(
+                                        "sl_sh_fn macro is broken, apparently ArgType::Exp can't be parsed as ArgType::Exp",
+                                    ),
+                                );
+                            }
+                        }
+                    }
+                    Some(arg_1) => {
+                        match arg_1.get().data {
+                            sl_sh::ExpEnum::Int(arg_1) => {
+                                let arg_1 = Some(arg_1);
+                                match arg_0 {
+                                    sl_sh::ArgType::Exp(arg_0) => {
+                                        match arg_0.get().data {
+                                            sl_sh::ExpEnum::Int(arg_0) => {
+                                                opt_int_to_float(arg_0, arg_1).map(Into::into)
+                                            }
+                                            _ => {
+                                                return Err(
+                                                    LispError::new(
+                                                        "sl_sh_fn macro is broken, ArgType::Exp can't be parsed as ArgType::Exp",
+                                                    ),
+                                                );
+                                            }
+                                        }
+                                    }
+                                    _ => {
+                                        return Err(
+                                            LispError::new(
+                                                "sl_sh_fn macro is broken, apparently ArgType::Exp can't be parsed as ArgType::Exp",
+                                            ),
+                                        );
+                                    }
+                                }
+                            }
+                            _ => {
+                                return Err(
+                                    LispError::new(
+                                        "sl_sh_fn macro is broken, ArgType::Opt can't be parsed as ArgType::Opt",
+                                    ),
+                                );
+                            }
+                        }
+                    }
+                }
+            }
+            _ => {
+                return Err(
+                    LispError::new(
+                        "sl_sh_fn macro is broken, apparently ArgType::Opt can't be parsed as ArgType::Opt",
+                    ),
+                );
+            }
+        }
+    }
+}
