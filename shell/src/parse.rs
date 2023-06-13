ccf7030640945a07609aad66257b9d418bde99f8
Steven Stanfield <stanfield@scarecrowtech.com>
2023-06-13 01:44:11 -0400

    Better double quoted string in the shell reader.

--- shell/src/parse.rs
+++ shell/src/parse.rs
@@ -409,6 +409,106 @@ fn read_simple_string(chars: &mut Peekable<Chars>) -> Result<Arg, io::Error> {
     Err(io::Error::new(ErrorKind::Other, "unclosed string"))
 }
 
+fn char_to_hex_num(ch: char) -> Result<u8, io::Error> {
+    if ch.is_ascii_digit() {
+        Ok(ch as u8 - b'0')
+    } else {
+        match ch {
+            'a' | 'A' => Ok(10),
+            'b' | 'B' => Ok(11),
+            'c' | 'C' => Ok(12),
+            'd' | 'D' => Ok(13),
+            'e' | 'E' => Ok(14),
+            'f' | 'F' => Ok(15),
+            _ => Err(io::Error::new(
+                ErrorKind::Other,
+                format!("Invalid hex digit {ch}, expected 0-9 or A-F."),
+            )),
+        }
+    }
+}
+
+/// Read an ascii char from 0x00-0x7F endowed in a string as '\xXX' where X is single hex digit.
+fn escape_to_char(chars: &mut Peekable<Chars>) -> Result<char, io::Error> {
+    if let (Some(ch1), Some(ch2)) = (chars.next(), chars.peek()) {
+        let ch_n: u8 = (char_to_hex_num(ch1)? * 16) + (char_to_hex_num(*ch2)?);
+        if ch_n > 0x7f {
+            Err(io::Error::new(
+                ErrorKind::Other,
+                "Invalid hex ascii code, must be less then \\x7f.".to_string(),
+            ))
+        } else {
+            Ok(ch_n as char)
+        }
+    } else {
+        Err(io::Error::new(
+            ErrorKind::Other,
+            "Invalid hex ascii code, expected two digits.".to_string(),
+        ))
+    }
+}
+
+/// Read a UTF8 codepoint encoded in a string with '\uXXXXXXXX' or \u{XXXXXXXX}' where X is a
+/// single hex digit.  There can be 1-8 hex values (X- a nibble) in the encoding (Up to 4 bytes).
+fn read_utf_scalar(chars: &mut Peekable<Chars>) -> Result<char, io::Error> {
+    fn finish(char_u32: u32) -> Result<char, io::Error> {
+        if let Some(val) = std::char::from_u32(char_u32) {
+            Ok(val)
+        } else {
+            Err(io::Error::new(
+                ErrorKind::Other,
+                format!("Invalid unicode scalar, {char_u32:x} not a valid utf scalar."),
+            ))
+        }
+    }
+    let mut first = true;
+    let mut has_bracket = false;
+    let mut char_u32 = 0;
+    let mut nibbles = 0;
+    let mut next_ch = chars.peek().copied();
+    while let Some(ch) = next_ch {
+        if ch == '\n' {
+            break;
+        }
+        if ch == '"' {
+            break;
+        }
+        if !has_bracket && ch.is_whitespace() {
+            return finish(char_u32);
+        }
+        if first && ch == '{' {
+            has_bracket = true;
+            first = false;
+            chars.next();
+            next_ch = chars.peek().copied();
+            continue;
+        }
+        first = false;
+        if has_bracket && ch == '}' {
+            return finish(char_u32);
+        }
+        if nibbles >= 8 {
+            return Err(io::Error::new(
+                ErrorKind::Other,
+                "Invalid unicode scalar, too many bytes (4 max).".to_string(),
+            ));
+        }
+        nibbles += 1;
+        let nib = char_to_hex_num(ch)?;
+        char_u32 = (char_u32 << 4) | nib as u32;
+        chars.next();
+        next_ch = chars.peek().copied();
+    }
+    if has_bracket {
+        Err(io::Error::new(
+            ErrorKind::Other,
+            "Invalid unicode scalar, failed to parse.".to_string(),
+        ))
+    } else {
+        finish(char_u32)
+    }
+}
+
 /// Read string surrounded by quote (").  Assumes chars is on the open quote and
 /// consumes the end quote.
 /// This version will handle interpolation and escape chars.
@@ -447,8 +547,80 @@ fn read_string(chars: &mut Peekable<Chars>) -> Result<Arg, io::Error> {
                 arg = Some(Arg::Compound(args));
             }
             next_ch = chars.peek().copied();
-        } else if ch == '\\' && chars.peek().copied() == Some('\n') {
-            // Consume \newline.
+        } else if ch == '\\' {
+            chars.next();
+            next_ch = chars.peek().copied();
+            match next_ch {
+                Some('\n') => {
+                    // Consume \newline.
+                }
+                Some('e') | Some('E') => {
+                    // Escape
+                    res.push('\x1B');
+                }
+                Some('n') => {
+                    // Linefeed
+                    res.push('\n');
+                }
+                Some('r') => {
+                    // Carriage Return
+                    res.push('\r');
+                }
+                Some('t') => {
+                    // Tab
+                    res.push('\t');
+                }
+                Some('a') => {
+                    // Bell
+                    res.push('\x07');
+                }
+                Some('b') => {
+                    // Backspace
+                    res.push('\x08');
+                }
+                Some('f') => {
+                    // Formfeed
+                    res.push('\x0C');
+                }
+                Some('v') => {
+                    // Vertical tab
+                    res.push('\x0B');
+                }
+                Some('\"') => {
+                    res.push('"');
+                }
+                Some('x') => {
+                    chars.next();
+                    let xch = escape_to_char(chars)?;
+                    res.push(xch);
+                }
+                Some('\\') => {
+                    res.push('\\');
+                }
+                Some('u') => {
+                    chars.next();
+                    let uch = read_utf_scalar(chars)?;
+                    res.push(uch);
+                    if let Some(ch) = chars.peek().copied() {
+                        // If a \u ends in whitespace need to keep it vs swallow it.
+                        if ch.is_whitespace() {
+                            res.push(ch);
+                        }
+                        // Need to see this quote to finish...
+                        if ch == '"' {
+                            next_ch = chars.peek().copied();
+                            continue;
+                        }
+                    }
+                }
+                Some(nch) => {
+                    res.push(ch);
+                    res.push(nch);
+                }
+                _ => {
+                    res.push('\\');
+                }
+            }
             chars.next();
             next_ch = chars.peek().copied();
         } else {
@@ -546,14 +718,6 @@ fn read_special_arg(chars: &mut Peekable<Chars>, end_char: Option<char>) -> Resu
         };
         if !name.is_empty() {
             args.push(Arg::Var(name.into()));
-        }
-    }
-    if !chars.peek().unwrap_or(&' ').is_whitespace() && chars.peek().copied() != end_char {
-        let next_arg = read_arg(chars, end_char)?;
-        if let Arg::Compound(mut nargs) = next_arg {
-            args.append(&mut nargs);
-        } else {
-            args.push(next_arg);
         }
     }
     Ok(if args.len() == 1 {
@@ -675,6 +839,12 @@ mod tests {
         assert_eq!(&pj.to_string(), expected);
     }
 
+    fn test_parse_once(input: &str, expected: &str) {
+        let pj = parse_line(input).unwrap();
+        let pj_str = pj.to_string();
+        assert_eq!(&pj_str, expected);
+    }
+
     #[test]
     fn test_basic_parse() {
         test_parse("ls", "ls");
@@ -717,5 +887,35 @@ mod tests {
             "(ls -al)|(grep May|grep 7)|grep 00",
             "(ls -al) | (grep May | grep 7) | grep 00",
         )
+    }
+
+    #[test]
+    fn test_strings() {
+        test_parse_once("\"one\\ntwo\"", "one\x0Atwo");
+        test_parse_once("'one\\ntwo'", "one\\ntwo");
+        test_parse_once("'one\ntwo'", "one\x0Atwo");
+        test_parse_once("\"one\\x0atwo\"", "one\x0Atwo");
+        test_parse_once("\"one\\x0Atwo\"", "one\x0Atwo");
+        test_parse_once("\"one\\u0a two\"", "one\x0A two");
+        test_parse_once("\"one\\u0A two\"", "one\x0A two");
+        test_parse_once("\"one\\u{0a}two\"", "one\x0Atwo");
+        test_parse_once("\"one\\u{0A}two\"", "one\x0Atwo");
+        test_parse_once("\"one\\vtwo\"", "one\x0Btwo");
+        test_parse_once("\"one\\ftwo\"", "one\x0Ctwo");
+        test_parse_once("\"one\\etwo\"", "one\x1Btwo");
+        test_parse_once("\"one\\Etwo\"", "one\x1Btwo");
+        test_parse_once("\"one\\atwo\"", "one\x07two");
+        test_parse_once("\"one\\btwo\"", "one\x08two");
+        test_parse_once("\"one\\ttwo\"", "one\x09two");
+        test_parse_once("\"one\\rtwo\"", "one\x0Dtwo");
+        test_parse_once("\"one\\\\rtwo\"", "one\\rtwo");
+        test_parse_once("\"one\\\"two\"", "one\"two");
+        test_parse_once("\"one\\x0a\"", "one\x0A");
+        test_parse_once("\"one\\u0a\"", "one\x0A");
+        test_parse_once("\"one\\u0a \"", "one\x0A ");
+        test_parse_once("\"one\\u{0a}\"", "one\x0A");
+        test_parse_once("\"one\\u{0a}\ntwo\"", "one\x0A\ntwo");
+        test_parse_once("\"one\\u0a\ntwo\"", "one\x0A\ntwo");
+        test_parse_once("\"one\\u0a\n\"", "one\x0A\n");
     }
 }
