dfa67f27e480f37ad4619313a4de5fc274a91091
gpwclark <gpwclark@gmail.com>
2023-12-08 15:56:09 -0500

    do not merge this code

--- .template/src/lib.rs
+++
@@ -1,1 +0,0 @@
-use proc_macro::TokenStream;
--- .template/tests/progress.rs
+++
@@ -1,9 +0,0 @@
-#[test]
-fn tests() {
-    let t = trybuild::TestCases::new();
-
-    // TODO: add tests
-    //
-    // t.pass("tests/01-something-that-works.rs");
-    // t.compile_fail("tests/02-some-compiler-error.rs");
-}
