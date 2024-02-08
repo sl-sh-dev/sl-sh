82c7d0207b3a9de0fd9bab2df2b7acb8505cfca0
gpwclark <gpwclark@gmail.com>
2024-02-08 12:25:05 +0100

    cargo fmt

--- compiler/src/test_utils.rs
+++ compiler/src/test_utils.rs
@@ -4,8 +4,8 @@ pub mod utils;
 // paths that start with crate:: (because test_utils/utils.rs can be
 // imported in any crate in the workspace and therefore should not
 // have any contextual use statements).
-use compile_state::state::{CompileState, SloshVm, SloshVmTrait};
-use slvm::{RET, Value};
-use std::sync::Arc;
-use crate::{compile, Reader, ReadError};
 use crate::pass1::pass1;
+use crate::{compile, ReadError, Reader};
+use compile_state::state::{CompileState, SloshVm, SloshVmTrait};
+use slvm::{Value, RET};
+use std::sync::Arc;
