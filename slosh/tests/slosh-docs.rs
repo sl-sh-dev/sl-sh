118a1f479132c7f6e3624f6fc640b8ccbbc7e7ad
gpwclark <gpwclark@gmail.com>
2024-02-22 23:47:17 -0500

    fix

--- slosh/tests/slosh-docs.rs
+++ slosh/tests/slosh-docs.rs
@@ -8,12 +8,6 @@ use std::process::Command;
 // This integration test exists to test all the functions that slosh has by default.
 // This differs from the unit tests that test the docs in the docs module
 
-// TODO PC ISSUE #111 I believe... an integration test could just run through every
-//  slosh file it finds in a directory, run it, and report if any errors occurred?
-//  issue is making sure any error in the slosh file actually reports a non zero exit
-//  code. Would be a good way to easily add slosh level tests with slosh scripts BUT
-//  have theam easily caught in CI as per issue.
-
 // TODO PC slosh -c broken?
 
 #[test]
@@ -26,26 +20,22 @@ use std::process::Command;
 ///
 fn test_slosh_doc_string_parsing_in_slosh() {
     let test_script = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("run-tests.slosh");
-    println!("test_script: {test_script:?}");
+    println!("Slosh doc test script: {test_script:?}");
 
     let slosh_path = PathBuf::from(env!("CARGO_BIN_EXE_slosh")).into_os_string();
     let slosh_path = slosh_path.to_str();
-    println!("curr_path: {slosh_path:?}");
-    println!("slosh_path: {slosh_path:?}");
-
     let output = Command::new(slosh_path.unwrap())
         .arg(test_script)
         .output()
         .expect("Failed to execute command");
 
     let s = String::from_utf8_lossy(output.stdout.as_slice());
-    println!("status {}", output.status);
-    println!("stdout {}", s);
+    println!("stdout:\n{}", s);
+    println!("status: {}", output.status);
 
     assert!(
         output.status.success(),
-        "Integration test script failed!: \nstdout:\n{}\n\nstderr:\n{}",
-        String::from_utf8_lossy(output.stdout.as_slice()),
+        "Integration test script failed!: stderr:\n{}",
         String::from_utf8_lossy(output.stderr.as_slice())
     );
 }
