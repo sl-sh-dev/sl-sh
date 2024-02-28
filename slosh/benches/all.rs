a4aec84b195fb60cc1d30aaa9e1d8133f01f0dfb
gpwclark <gpwclark@gmail.com>
2024-02-28 00:02:34 -0500

    try this

--- slosh/benches/all.rs
+++ slosh/benches/all.rs
@@ -1,7 +1,4 @@
-#[cfg(not(target_arch = "x86_64"))]
-use criterion::{criterion_group, criterion_main};
-#[cfg(target_arch = "x86_64")]
-use iai::main;
+use std::path::PathBuf;
 
 use compile_state::state::{new_slosh_vm, CompileState, SloshVm, SloshVmTrait};
 use sl_compiler::pass1::pass1;
@@ -46,32 +43,6 @@ fn load_one_expression(
     Ok((Arc::new(state.chunk), state.doc_string))
 }
 
-pub const FLOAT_BENCH: &'static str = r#"
-(def defmacro
-  (macro (name args & body)
-      `(def ~name (macro ~args ~@body))))
-(defmacro dotimes-i
-    (idx-bind times & body)
-    `(let (~idx-bind 0)
-        (while (< ~idx-bind ~times)
-            ~@body
-            (inc! ~idx-bind))))
-(def eval-pol (fn (n x)
-  (let (su 0.0
-        mu 10.0
-        pu 0.0
-        pol (make-vec 100 0.0))
-    (dotimes-i i n
-        (set! su 0.0)
-        (dotimes-i j 100
-             (set! mu (/ (+ mu 2.0) 2.0))
-             (set! pol.~j mu))
-        (dotimes-i j 100
-          (set! su (+ pol.~j (* su x))))
-            (set! pu (+ pu su)))
-    pu)))
-"#;
-
 pub fn run_bench(n: usize, m: f32) -> String {
     format!("(eval-pol {} {})", n, m)
 }
@@ -94,11 +65,29 @@ pub fn run_reader(reader: &mut Reader) -> VMResult<Value> {
     Ok(last)
 }
 
+/// returns ./slosh/benches/ PathBuf
+pub fn get_benches_directory() -> PathBuf {
+    let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    path.push("benches");
+    path
+}
+
+pub fn get_float_benchmark() -> PathBuf {
+    get_benches_directory().join("float-bench.slosh")
+}
+
 fn run_float_script(n: usize, m: f32, expected: f32) {
     let vm = &mut new_slosh_vm();
-    let mut reader = Reader::from_static_string(FLOAT_BENCH, vm, "", 1, 0);
-    _ = run_reader(&mut reader);
-
+    let fname = get_float_benchmark();
+    match std::fs::File::open(&fname) {
+        Ok(file) => {
+            let mut reader = Reader::from_file(file, vm, "", 1, 0);
+            let _ = run_reader(&mut reader);
+        }
+        Err(e) => {
+            panic!("Could not open file: {:?}", e);
+        }
+    }
     let mut reader = Reader::from_string(run_bench(n, m), vm, "", 1, 0);
     let last = run_reader(&mut reader);
     match last {
@@ -114,7 +103,7 @@ fn run_float_script(n: usize, m: f32, expected: f32) {
 #[cfg(target_arch = "x86_64")]
 mod instruction_count {
     use super::*;
-    use iai::black_box;
+    use iai::{black_box, main};
 
     fn float_one_hundred() {
         black_box(run_float_script(100, 0.5, 400.0));
@@ -127,45 +116,55 @@ mod instruction_count {
     fn float_ten_thousand() {
         black_box(run_float_script(10_000, 0.2, 25000.0));
     }
-}
 
-#[cfg(target_arch = "x86_64")]
-main!(
-    instruction_count::float_one_hundred,
-    instruction_count::float_one_thousand
-    instruction_count::float_ten_thousand
-);
+    main!(float_one_hundred, float_one_thousand, float_ten_thousand,);
+
+    pub fn run() {
+        main();
+    }
+}
 
 #[cfg(not(target_arch = "x86_64"))]
 mod wall_clock {
     use super::*;
-    use criterion::Criterion;
+    use criterion::{criterion_group, criterion_main, Criterion};
 
-    pub fn float_one_hundred(c: &mut Criterion) {
+    fn float_one_hundred(c: &mut Criterion) {
         c.bench_function("float_ten", |bench| {
             bench.iter(|| std::hint::black_box(run_float_script(100, 0.5, 400.0)));
         });
     }
 
-    pub fn float_one_thousand(c: &mut Criterion) {
+    fn float_one_thousand(c: &mut Criterion) {
         c.bench_function("float_one_thousand", |bench| {
             bench.iter(|| std::hint::black_box(run_float_script(1000, 0.05, 2105.2483)));
         });
     }
 
-    pub fn float_ten_thousand(c: &mut Criterion) {
+    fn float_ten_thousand(c: &mut Criterion) {
         c.bench_function("float_one_hundred", |bench| {
             bench.iter(|| std::hint::black_box(run_float_script(10_000, 0.2, 25000.0)));
         });
     }
+
+    criterion_group!(
+        benches,
+        float_one_hundred,
+        float_one_thousand,
+        float_ten_thousand,
+    );
+
+    criterion_main!(benches);
+
+    pub fn run() {
+        main();
+    }
 }
 
-#[cfg(not(target_arch = "x86_64"))]
-criterion_group!(
-    benches,
-    wall_clock::float_one_hundred,
-    wall_clock::float_one_thousand,
-    wall_clock::float_ten_thousand,
-);
-#[cfg(not(target_arch = "x86_64"))]
-criterion_main!(benches);
+fn main() {
+    #[cfg(target_arch = "x86_64")]
+    instruction_count::run();
+
+    #[cfg(not(target_arch = "x86_64"))]
+    wall_clock::run();
+}
