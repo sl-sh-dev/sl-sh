cb8daea96dc94229da0d7403a8c0fdd29a0e817f
gpwclark <gpwclark@gmail.com>
2024-04-03 18:11:08 -0400

    raw html of legacy site

--- jekyll.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-bundle exec jekyll  serve --config  _config.yml,_user_config.yml
--- serve-local-docs.lisp
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sl-sh
-
-(ns-import 'shell)
-
-$(bundle exec jekyll serve --config (glob "~/development/sl-sh-dev/sl-sh/docs/_user_config.yml"))
-
