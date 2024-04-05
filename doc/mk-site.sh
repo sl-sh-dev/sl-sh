1478c84f1203fe4a25e53ced08a5530bd97e425c
gpwclark <gpwclark@gmail.com>
2024-04-05 15:01:42 -0400

    fix preprocessing

--- doc/mk-site.sh
+++ doc/mk-site.sh
@@ -7,7 +7,7 @@ git clone -b gh-pages-legacy-html https://github.com/sl-sh-dev/sl-sh src/legacy/
 rm -rf src/legacy/.git
 mkdir src/rust-docs
 
-cargo doc --target-dir src/rust-docs
+cargo doc --features lisp-test --target-dir src/rust-docs
 
 cargo build --features "lisp-test"
 
