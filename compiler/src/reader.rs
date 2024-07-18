9253c285559be8fcd45c2f4d1d0052a382fe7e27
Steven Stanfield <stanfield@scarecrowtech.com>
2024-07-17 21:30:57 -0400

    Fix benches (?).

--- compiler/src/reader.rs
+++ compiler/src/reader.rs
@@ -515,6 +515,7 @@ impl<'vm> Reader<'vm> {
                         symbol.push(res);
                     }
                     _ => {
+                        println!("XXXXX pushing {ch}");
                         symbol.push_str(&ch);
                     }
                 }
