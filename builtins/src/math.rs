053d39a399a8d25738401d8b54469398968ccb78
Steven Love <lovestevend@gmail.com>
2024-09-20 15:05:01 -0700

    feat: implemented forms '%' and 'rem_euclid' [skip benchmarks]

--- builtins/src/math.rs
+++ builtins/src/math.rs
@@ -33,6 +33,74 @@ pub fn abs(v: Value) -> VMResult<Value> {
     }
 }
 
+/// Usage: (% int int)
+///
+/// Remainder from dividing (arg 1) by (arg 2).
+/// Note: Remainder and Modulo are two similar mathematical operations,
+/// called `rem` and `rem_euclid` in Rust.
+/// This function uses `rem` which is the same as the `%` operator in C.
+/// With `rem`, the sign of the result is the same as the dividend (arg 1).
+/// With `rem_euclid`, the sign of the result is always positive.
+///
+/// Section: math
+///
+/// Example:
+/// (test::assert-equal 0 (% 50 10))
+/// (test::assert-equal 5 (% 55 10))
+/// (test::assert-equal 1 (% 1 2))
+/// (test::assert-equal -1 (% -10 3))
+/// (test::assert-equal  1 (% 10 -3))
+/// (test::assert-equal -1 (% -10 -3))
+///
+/// (test::assert-error (%))
+/// (test::assert-error (% 1))
+/// (test::assert-error (% 1 2 3))
+/// (test::assert-error (% 1 2.0))
+#[sl_sh_fn(fn_name = "%")]
+pub fn rem(dividend: i64, divisor: i64) -> VMResult<i64> {
+    dividend.checked_rem(divisor).ok_or_else(|| {
+        VMError::new_vm(format!(
+            "rem: division by zero or overflow: {} % {}",
+            dividend, divisor
+        ))
+    })
+}
+
+/// Usage: (rem_euclid int int)
+///
+/// Least Non-negative number that can be added to a multiple of the divisor (arg 2) to get the dividend (arg 1).
+/// The result should always be 0 <= result < divisor (arg 2).
+/// Note: Remainder and Modulo are two similar mathematical operations,
+/// called `rem` and `rem_euclid` in Rust.
+/// With `rem`, the sign of the result is the same as the dividend (arg 1).
+/// With `rem_euclid`, the sign of the result is always positive.
+///
+/// Section: math
+///
+/// Example:
+/// (test::assert-equal 0 (rem_euclid 50 10))
+/// (test::assert-equal 5 (rem_euclid 55 10))
+/// (test::assert-equal 1 (rem_euclid 1 2))
+/// (test::assert-equal 2 (rem_euclid -10 3))
+/// (test::assert-equal 1 (rem_euclid 10 -3))
+/// (test::assert-equal 2 (rem_euclid -10 -3))
+///
+/// (test::assert-error (rem_euclid))
+/// (test::assert-error (rem_euclid 1))
+/// (test::assert-error (rem_euclid 1 2 3))
+/// (test::assert-error (rem_euclid 1 2.0))
+#[sl_sh_fn(fn_name = "rem_euclid")]
+pub fn rem_euclid(dividend: i64, divisor: i64) -> VMResult<i64> {
+    dividend.checked_rem_euclid(divisor).ok_or_else(|| {
+        VMError::new_vm(format!(
+            "rem_euclid: division by zero or overflow: {} % {}",
+            dividend, divisor
+        ))
+    })
+}
+
 pub fn add_math_builtins(env: &mut SloshVm) {
     intern_abs(env);
+    intern_rem(env);
+    intern_rem_euclid(env);
 }
