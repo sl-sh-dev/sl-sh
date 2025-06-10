6e16962ff97cd642510135648b8cebe0e6301364
gpwclark <gpwclark@gmail.com>
2025-06-10 02:46:41 -0400

    update

--- bridge_adapters/src/lib.rs
+++ bridge_adapters/src/lib.rs
@@ -1,5 +1,5 @@
 use compile_state::state::{CompileEnvironment, SloshVm, SloshVmTrait};
-use slvm::CallFuncSig;
+use slvm::{CallFuncSig, Value};
 
 pub mod lisp_adapters;
 
@@ -10,6 +10,18 @@ pub fn add_builtin(
     doc_string: &str,
 ) {
     let si = env.set_global_builtin(name, func);
+    let key = env.intern("doc-string");
+    let s = env.alloc_string(doc_string.to_string());
+    env.set_global_property(si, key, s);
+}
+
+pub fn add_named_global_doc(
+    env: &mut SloshVm,
+    string: &str,
+    f_val: Value,
+    doc_string: &str,
+) {
+    let si = env.set_named_global(string, f_val);
     let key = env.intern("doc-string");
     let s = env.alloc_string(doc_string.to_string());
     env.set_global_property(si, key, s);
